f75b1df0afafbfb85d4b7a26a05b9855ca6fe667
root <root@DESKTOP-PI8J1VA>
2024-03-18 14:33:37 +0530

    update files name

---
+++ src/helloworld.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+contract helloworld {
+    string public Welcome;
+
+    constructor() {
+        Welcome = "hello, Welcome to the web3 world";
+    }
+}
