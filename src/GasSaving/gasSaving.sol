6d14509b1f8c3ac4f55797adac6c0d31ff8ce255
root <root@DESKTOP-PI8J1VA>
2024-03-19 15:38:31 +0530

    Immutable

--- src/GasSaving/gasSaving.sol
+++ src/GasSaving/gasSaving.sol
@@ -1,11 +1,16 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.25;
+pragma solidity ^0.8.24;
 
 contract gasSaving {
     //Constants are variables that cannot be modified.
-    //Their value is hard coded and using constants can save gas cos
+    //Their value is hard coded and using constants can save gas cost
 
-    //
     uint256 public constant b = 100;
     uint256 public immutable a;
+
+    //Immutable variables are like constants. Values of immutable variables can be set inside the constructor but cannot be modified afterwards.
+
+    constructor() {
+        a = 100;
+    }
 }
