bac4917bb89718f9a5c428f54442dcabd44e5a1c
root <root@DESKTOP-PI8J1VA>
2024-03-19 18:12:39 +0530

    condition and loops

--- src/Condition & Loops/Condition.sol
+++ src/Condition & Loops/Condition.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
-contract Condition {
+contract Condition1 {
     //if,   if.....else, if....if else...else
     function greterThan(uint256 a) public pure returns (string memory) {
         string memory x;
@@ -25,5 +25,29 @@ contract Condition {
             z = "B is less than 10";
         }
         return z;
+    }
+}
+
+contract Condition2 {
+    //for , while & do while loop.....
+    uint256 count;
+    uint256[] arr;
+
+    //for loop
+    function oneTohundred(uint256 b) public returns (uint256) {
+        for (uint256 i = 1; i < b; i++) {
+            count += i;
+        }
+        return count;
+    }
+
+    //do ...while loop
+    function hundred() public returns (uint256[] memory) {
+        uint256 j = 1;
+        do {
+            arr[j] = j;
+            j++;
+        } while (j < 100);
+        return arr;
     }
 }
