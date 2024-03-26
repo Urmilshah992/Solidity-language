c6ab964728685e789ff95b4d80a57844a19e9ffd
root <root@DESKTOP-PI8J1VA>
2024-03-26 09:55:52 +0530

    array update

---
+++ src/Array/array.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIt
+pragma solidity ^0.8.24;
+
+contract array {
+    //There are two types of array
+    //1) fix sized array
+    //2) dynamic sized array.
+
+    uint256[] public dynamicarr;
+    uint256[5] public staticarr;
+    string[5] public strarr;
+    address[10] addressarr;
+
+    uint[] public fixValuearr = [1, 2, 3, 4, 5];
+
+    function adddynamicvalue(uint256 index) public {
+        dynamicarr.push(index);
+    }
+
+    function removefixValuearr(uint256 rm) public {
+        fixValuearr[rm] = fixValuearr[fixValuearr.length - 1];
+        fixValuearr.pop();
+    }
+}
