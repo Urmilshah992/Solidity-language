770123953d09202d8f8691d02bf69312f55894b7
Urmilshah992 <160323398+Urmilshah992@users.noreply.github.com>
2024-03-30 17:49:38 +0530

    Interface Logic Add

---
+++ src/Interface/interface.sol
@@ -0,0 +1,50 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+//You can interact with other contracts by decalaring an Interface.
+//Interface
+
+//1) Cannot have any function implemented.
+//2) Can inherit from other function
+//3) All declared function must be external
+//4) Cannot declared constructor
+//5) Cannot declared state variable.
+
+contract counter {
+    uint256 public count;
+
+    function increament() external {
+        count += 1;
+    }
+}
+
+interface Icounter {
+    function count() external view returns (uint256);
+
+    function increment() external;
+}
+
+contract MyContract {
+    function incrementcounter(address _counter) external {
+        Icounter(_counter).increment();
+    }
+
+    function getcount(address _counter) external view returns (uint256) {
+        return Icounter(_counter).count();
+    }
+}
+
+// Second Example
+
+interface calculater {
+    function getResult() external view returns (uint256);
+}
+
+contract test is calculater {
+    function getResult() external pure returns (uint256) {
+        uint256 a = 1;
+        uint256 b = 2;
+        uint256 result = a + b;
+        return result;
+    }
+}
